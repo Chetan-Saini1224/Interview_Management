4bbfc390026e41c1c9362e726f9fd0b899a45ce6
Chetan Saini <cs75198404@gmail.com>
2023-03-31 00:50:15 +0530

    fix signup redirect

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,51 +1,49 @@
-import User from "../models/user.js"
+import User from "../models/user.js";
 
-export function signUp(req,res)
-{
-    return res.render("signup",{title:"Create Account"});
+export function signUp(req, res) {
+  return res.render("signup", { title: "Create Account" });
 }
 
-export function signIn(req,res)
-{
-    return res.render("login",{title:"Login Page"});
+export function signIn(req, res) {
+  return res.render("login", { title: "Login Page" });
 }
 
-export async function createAccount(req,res) 
-{
-    try{
+export async function createAccount(req, res) {
+  try {
     console.log(req.body);
-    const exist = await User.findOne({email:req.body.email});
-    if(!exist) 
-    {
-        await User.create(req.body);
-        return res.redirect(signIn);
+    const exist = await User.findOne({ email: req.body.email });
+    if (!exist) {
+      await User.create(req.body);
+      return res.status(200).json({
+        success: "User Created",
+        location: "/user/login",
+      });
+    } else {
+      return res.status(200).json({
+        success: "User Already exist..",
+      });
     }
-    return res.status(200).json({
-        success:"User Already exist.."
-    });
-   }
-   catch(err){
-    console.log(err)
+  } catch (err) {
+    console.log(err);
     return res.status(500).json({
-        error:"Server Error...."
+      error: "Server Error....",
     });
-   }
+  }
 }
 
-export  function loginUser(req,res) 
-{
+export function loginUser(req, res) {
   return res.redirect("/");
 }
 
+export function logOut(req, res) {
+  //passport give this function to req
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "You have logged out!");
 
-export function logOut(req,res)
-{ 
-   //passport give this function to req 
-   req.logout(function(err) {
-      if (err) { return next(err); }
-      req.flash('success','You have logged out!');
-
-      //we can also send context like logout successfully.but eeeee...
-      res.redirect('/');
-    });
+    //we can also send context like logout successfully.but eeeee...
+    res.redirect("/");
+  });
 }
